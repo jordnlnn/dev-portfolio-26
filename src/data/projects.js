e7adbc5f558ba1a0bc48caac982568aa32d39349
jordnlnn <80535966+jordnlnn@users.noreply.github.com>
2026-01-09 19:07:16 -0700

    Add links to project buttons

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,8 +4,8 @@ export const projects = [
     description:
       "A friendly, interactive web app that turns your favorite Pokémon into a  personal emotional-support companion—powered by Google Gemini and  PokeAPI. Built with React.js, JSX, CSS.",
     links: {
-      live: "https://YOUR-LIVE-LINK",
-      code: "https://YOUR-CODE-LINK",
+      live: "https://pokemon-support-buddy.netlify.app/",
+      code: "https://github.com/jordnlnn/pokemon-support-buddy",
     },
   },
   {
@@ -13,7 +13,7 @@ export const projects = [
     description:
       "A responsive web application that allows users to search and explore word definitions. Powered by Free Dictionary API & Pexels API. Built with React.js, JSX, & CSS.",
     links: {
-      live: "https://YOUR-LIVE-LINK",
+      live: "https://zealous-bartik-1e08ce.netlify.app/",
       code: "https://github.com/jordnlnn/react-dictionary-project",
     },
   },
@@ -22,8 +22,8 @@ export const projects = [
     description:
       "A responsive web application that displays real-time timezones with visual day/night states, built to explore time-based UI logic and dynamic updates. Built with HTML, CSS, JavaScript, Moment.js.",
     links: {
-      live: "https://YOUR-LIVE-LINK",
-      code: "https://YOUR-CODE-LINK",
+      live: "https://jordnlnn-world-clock.netlify.app/",
+      code: "https://github.com/jordnlnn/world-clock",
     },
   },
 ];
